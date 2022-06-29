9edb43ed57aa34032f673a0e56ab44f1d8bce809
Pedro Lima <pedrohblima03@gmail.com>
2022-06-29 03:49:26 -0300

    .

---
+++ script.js
@@ -0,0 +1,33 @@
+const slideItems = document.querySelectorAll('.slider-item');
+let activeItem = 0;
+const btnNext = document.getElementById('btnNext');
+const btnPrev = document.getElementById('btnPrev');
+const sliderWrapper = document.getElementById('sliderWrapper')
+
+btnNext.addEventListener('click', (e) => {
+  if (activeItem == slideItems.length - 1){
+    activeItem = 0;
+  }
+  else {
+    activeItem = activeItem + 1;
+  }
+
+  return slideControls(activeItem)
+})
+
+btnPrev.addEventListener('click', (e) => {
+  if (activeItem == 0){
+    activeItem = slideItems.length - 1;
+  }
+
+  else {
+    activeItem = activeItem - 1;
+  }
+
+  return slideControls(activeItem)
+})
+
+
+const slideControls = (item) => {
+  sliderWrapper.style.transform = `translateX(${-item * 100}%)`
+}
